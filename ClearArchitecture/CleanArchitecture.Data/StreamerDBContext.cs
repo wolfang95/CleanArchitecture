897f8bea39f947ea2156b9034f586a583389d942
Wolfang corredor <you@example.com>
2022-08-12 19:53:05 +0100

    Initial load

---
+++ ClearArchitecture/CleanArchitecture.Data/StreamerDBContext.cs
@@ -0,0 +1,21 @@
+﻿
+
+using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDBContext : DbContext
+    {
+        // Se convierte las clases de c# (streamer y video) cómo entidades
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost; 
+                Initial Catalog=Streamer;user id=sa;password=admin123"); //cadena de conexión
+        }
+        public DbSet<Streamer>? Streamers {get; set;}
+        public DbSet <Video>? Videos { get; set;}
+ 
+    }
+}
---
+++ ClearArchitecture/CleanArchitecture.Domain/Video.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain
+{
+    public class Video
+    {
+        public  int Id { get; set; }
+        public string? Name { get; set; }
+
+        public int? StrimerId { get; set; }
+        public virtual Streamer? Streamer { get; set; }
+    }
+}
