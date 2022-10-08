9593c8fda604b15220cecac68e8b732993de7b94
Wolfang corredor <you@example.com>
2022-10-08 16:43:03 +0200

    update clean architecture

---
+++ ClearArchitecture - First/CleanArchitecture.Domain/Streamer.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchitecture.Domain.Common;
+namespace CleanArchitecture.Domain
+{
+    public class Streamer : BaseDomainModel
+    {
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+        public ICollection<Video>? Videos { get; set; }
+
+    }
+}
